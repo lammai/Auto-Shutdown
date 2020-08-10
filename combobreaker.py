3f464230ef772252fe3a89aa4a7c3ac192f45b32
Liam Maes <lam7.2521@gmail.com>
2020-08-10 01:11:11 -0700

    modified:   combobreaker.py

--- combobreaker.py
+++ combobreaker.py
@@ -1,5 +1,6 @@
-# Simple script based off of Samy Kamkar's work on cracking combo lock
-# Made to be used on a terminal
+#! /usr/bin/python
+# Simple script based off of 
+# Samy Kamkar's work on cracking Master combo lock
 
 import math
 
@@ -17,7 +18,7 @@ for i in range(0, 40, 10):
 
 modFour = set([])
 possible2nd = []
-for x in range(0, 40):
+for x in range(0, 42):
     if (x % 4 == firstDigit % 4):
         modFour.add(x)
         if (x-2 >= 0):
@@ -39,14 +40,13 @@ except ValueError:
         if (possible2nd.count(0) == 1):
             possible2nd.remove(0)
         possible2nd.remove(thirdDigit + 2)
-    elif (thirdDigit + 2 > 39):
+    elif (thirdDigit + 2 >= 39):
+        possible2nd.remove(thirdDigit + 2 - 40)
         if (possible2nd.count(0) == 1):
             possible2nd.remove(0)
-        possible2nd.remove(thirdDigit - 2)
 
 print("*****************<RESULT>******************")
 print("1st Digit:", firstDigit)
 print("2nd Digits:", possible2nd)
 print("3rd Digits:", thirdDigit)
 print("<=========================================>")
-
